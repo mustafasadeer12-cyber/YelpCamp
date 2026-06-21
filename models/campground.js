b68b7e8090311955749bae5148d05d13a47d05f7
mustafasadeer12-cyber <mustafasadeer12@gmail.com>
2026-06-20 17:32:46 -0700

    added display images to edit form

--- models/campground.js
+++ models/campground.js
@@ -3,13 +3,18 @@ const Schema = mongoose.Schema;
 const Review = require ('./review')
 
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+
+})
+
+ImageSchema.virtual('thumbnail').get(function() {
+    return this.url.replace('/upload', '/upload/w_200')
+})
 const CampgroundSchema = new Schema ({
     title: String,
-    images: [
-        {url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     price: {
         type: Number,
         min: [0]},
