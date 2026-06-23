207212e42eacaa9a15f6ec9f337aa1f0c832f44e
mustafasadeer12-cyber <mustafasadeer12@gmail.com>
2026-06-22 19:59:33 -0700

    Styled Login & Register pages

--- controllers/users.js
+++ controllers/users.js
@@ -45,6 +45,6 @@ module.exports.logout = (req, res, next) => {
             return next(err);
         }
         req.flash('success', 'Goodbye!');
-        res.redirect('/campgrounds');
+        res.redirect('/');
     });
 }
